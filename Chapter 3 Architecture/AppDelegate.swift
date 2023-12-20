3a8b00a607856310d3c5f19ef6d773d0af0952da
Kishan <kmanvar@elluminatiinc.com>
2023-12-20 16:01:19 +0530

    Done

--- Chapter 3 Architecture/AppDelegate.swift
+++ Chapter 3 Architecture/AppDelegate.swift
@@ -16,8 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
-        // bug fix
-        
     }
     
     // MARK: UISceneSession Lifecycle
@@ -29,4 +27,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
-//main code added
